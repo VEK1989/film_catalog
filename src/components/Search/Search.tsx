861b64033aab869784ec780084397b4388199491
Vitaly <v.kononov@chillicode.ru>
2022-03-04 09:30:26 +0300

    fixed search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,15 +3,14 @@ import searchImg from "../../assets/images/header-search.png";
 import { searchActionCreators } from "../../store/action-creators/searchActionCreators";
 import { useDispatch } from "react-redux";
 import { useDebouncedCallback } from "use-debounce";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Search = () => {
   const dispatch = useDispatch();
-  const searchRef = useRef<HTMLInputElement>(null);
   const [selectValue, setSelectValue] = useState<string>("popular");
+  const [inputValue, setInputValue] = useState<string>("");
 
-  const handleOnNameChange = useDebouncedCallback(() => {
-    const value = searchRef.current?.value || "";
+  const handleOnNameChange = useDebouncedCallback((value) => {
     dispatch(searchActionCreators.setSearchName(value));
   }, 1500);
 
@@ -19,16 +18,19 @@ export const Search = () => {
     dispatch(searchActionCreators.setFilterChange(selectValue));
   }, [selectValue, dispatch]);
 
+  useEffect(() => {
+    handleOnNameChange(inputValue);
+  }, [inputValue, handleOnNameChange]);
+
   return (
     <div className={style.searchArea}>
       <div className={style.input}>
         <input
-          value={searchRef.current?.value}
-          ref={searchRef}
+          value={inputValue}
           type="text"
           className={style.input_area}
           placeholder="Search"
-          onChange={handleOnNameChange}
+          onChange={(e) => setInputValue(e.target.value)}
         />
         <img src={searchImg} alt="search" width="16px" height="16px" />
       </div>
