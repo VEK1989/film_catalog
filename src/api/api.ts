1f359fa61748456510ba72a094ad022c479977f9
Vitaly <v.kononov@chillicode.ru>
2022-03-04 12:35:18 +0300

    fixed env

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ import { GenresType } from "../store/types/genresType";
 import { ItemType } from "../store/types/ItemType";
 
 const instans = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: `${process.env.REACT_APP_BASE_URL}`,
 });
 
 export class FilmsDataApi {
@@ -14,7 +14,7 @@ export class FilmsDataApi {
     name: string
   ): Promise<AxiosResponse<ItemResultType>> {
     const response = await instans.get<ItemResultType>(
-      `${name}/${value}?api_key=${process.env.API_KEY}&language=${process.env.LANG_ENG}&page=${page}`
+      `${name}/${value}?api_key=${process.env.REACT_APP_API_KEY}&language=${process.env.REACT_APP_LANG_ENG}&page=${page}`
     );
     return response;
   }
@@ -24,7 +24,7 @@ export class FilmsDataApi {
     name: string
   ): Promise<AxiosResponse<FilmPropertiesType>> {
     const response = await instans.get<FilmPropertiesType>(
-      `${name}/${filmId}?api_key=${process.env.API_KEY}&language=${process.env.LANG_ENG}`
+      `${name}/${filmId}?api_key=${process.env.REACT_APP_API_KEY}&language=${process.env.REACT_APP_LANG_ENG}`
     );
     return response;
   }
@@ -35,7 +35,7 @@ export class FilmsDataApi {
     name: string
   ): Promise<AxiosResponse<ItemResultType>> {
     const response = await instans.get<ItemResultType>(
-      `search/${name}?api_key=${process.env.API_KEY}&query=${query}&page=${page}`
+      `search/${name}?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&page=${page}`
     );
     return response;
   }
@@ -44,7 +44,7 @@ export class FilmsDataApi {
     name: string
   ): Promise<AxiosResponse<GenresResponseType>> {
     const response = await instans.get<GenresResponseType>(
-      `genre/${name}/list?api_key=${process.env.API_KEY}&language=${process.env.LANG_ENG}`
+      `genre/${name}/list?api_key=${process.env.REACT_APP_API_KEY}&language=${process.env.REACT_APP_LANG_ENG}`
     );
     return response;
   }
